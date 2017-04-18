d2043692a41918ce4128f97fe10e0a8d3c8442a6
Altaf Alam <altaf@springpeople.com>
2017-04-18 19:14:04 +0530

    Include Foundation header

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,15 +1,34 @@
 var React = require('react');
 var {Link, IndexLink} = require('react-router');
 
-var Nav = () => {
-  return (
-    <div>
-      <h2>Nav Component</h2>
-      <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
-      <Link to="/about" activeClassName="active"  activeStyle={{fontWeight: 'bold'}}>About</Link>
-      <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
-    </div>
-  );
-};
+var Nav = React.createClass ({
+  onSearch: function(e){
+    e.preventDefault();
+    alert('its working');
+  },
+  render: function (){
+    return (
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">Site Title</li>
+            <li><IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink></li>
+            <li><Link to="/about" activeClassName="active"  activeStyle={{fontWeight: 'bold'}}>About</Link></li>
+            <li><Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link></li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <form onSubmit={this.onSearch}>
+            <ul className="menu">
+              <li><input type="text" placeholder="Enter City" /></li>
+              <li><input type="submit" className="button" value="Get Weather"/></li>
+            </ul>
+          </form>
+        </div>
+      </div>
+    );
+  }
+
+});
 
 module.exports = Nav;
