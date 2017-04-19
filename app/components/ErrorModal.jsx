e3eb0455c998e57d63a6705bc344f59cf060340e
Altaf Alam <altaf@springpeople.com>
2017-04-19 14:17:32 +0530

    Model Updated

---
+++ app/components/ErrorModal.jsx
@@ -0,0 +1,35 @@
+var React = require('react');
+
+var ErrorModal = React.createClass({
+  getDefaultProps: function(){
+    return{
+      title: 'Error'
+    };
+  },
+  propTypes: {
+    title: React.PropTypes.string,
+    message: React.PropTypes.string.isRequired
+  },
+  componentDidMount: function(){
+    var modal = new Foundation.Reveal($('#error-modal'));
+    modal.open();
+  },
+
+  render: function(){
+    var {title, message} = this.props;
+    return (
+      <div className="reveal tiny text-center data-animation-in" id="error-modal" data-reveal="">
+        <h1>{title}</h1>
+        <p className="lead">{message}</p>
+        <button className="close-button" data-close="" aria-label="Close modal" type="button">
+          <span aria-hidden="true">&times;</span>
+        </button>
+        <button className="button hollow" data-close="">
+          Okay!
+        </button>
+      </div>
+    );
+  }
+});
+
+module.exports = ErrorModal;
